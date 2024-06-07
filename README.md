6cebb423699245858dd6628041e9480ab4538615
yueltzehaie <168153788+yueltzehaie@users.noreply.github.com>
2024-06-07 15:09:33 +0200

    push new initial version - flutter

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # portfolio_-5450543-
+# flutter create my_new_project
+# flutter create portfolio_5450543
---
+++ portfolio_5450543/android/app/src/main/kotlin/de/thm/portfolio_5450543/MainActivity.kt
@@ -0,0 +1,5 @@
+package de.thm.portfolio_5450543
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
